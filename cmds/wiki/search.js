49b6c0dc533d82919ba6c5ec956a8cdd66fc67be
stackd <144233917+snowfleaf@users.noreply.github.com>
2025-07-07 21:08:50 +0800

    Use components V2

--- cmds/wiki/search.js
+++ cmds/wiki/search.js
@@ -1,8 +1,13 @@
-import { EmbedBuilder } from 'discord.js';
+import {
+  ContainerBuilder,
+  TextDisplayBuilder,
+  MessageFlags
+} from 'discord.js';
+
 import { botLimits } from '../../util/defaults.js';
 import { got, escapeFormatting, splitMessage } from '../../util/functions.js';
 
-const {search: searchLimit} = botLimits;
+const { search: searchLimit } = botLimits;
 
 /**
  * Searches a wiki.
@@ -14,128 +19,136 @@ const {search: searchLimit} = botLimits;
  * @param {import('discord.js').MessageReaction} reaction - The reaction on the message.
  * @param {String} spoiler - If the response is in a spoiler.
  * @param {Boolean} noEmbed - If the response should be without an embed.
- * @returns {Promise<{reaction?: WB_EMOJI, message?: String|import('discord.js').MessageOptions}>}
+ * @returns {Promise<{reaction?: WB_EMOJI, message?: Object}>}
  */
 export default function mw_search(lang, msg, searchterm, wiki, query, reaction, spoiler, noEmbed) {
 	if ( searchterm.length > 250 ) {
 		searchterm = searchterm.substring(0, 250).trim();
-		msg?.fetchReply?.().then( message => message?.reactEmoji?.(WB_EMOJI.warning), log_error );
+		msg?.fetchReply?.().then(message => message?.reactEmoji?.(WB_EMOJI.warning), console.error);
 		msg?.reactEmoji?.(WB_EMOJI.warning);
 	}
-	var pagelink = wiki.toLink('Special:Search', {search:searchterm,fulltext:1});
-	var resultText = '<' + pagelink + '>';
-	var embed = null;
-	if ( !noEmbed ) embed = new EmbedBuilder().setAuthor( {name: query.general.sitename} ).setTitle( '`' + searchterm + '`' ).setURL( pagelink );
-	else resultText += '\n\n**`' + searchterm + '`**';
-	var querypage = ( Object.values(( query.pages || {} ))?.[0] || {title:'',ns:0,invalid:''} );
-	var limit = searchLimit[( patreonGuildsPrefix.has(msg.guildId) ? 'patreon' : 'default' )];
-	return got.get( wiki + 'api.php?action=query&titles=Special:Search&list=search&srinfo=totalhits&srprop=redirecttitle|sectiontitle&srnamespace=4|12|14|' + ( querypage.ns >= 0 ? querypage.ns + '|' : '' ) + wiki.namespaces.content.map( ns => ns.id ).join('|') + '&srlimit=' + limit + '&srsearch=' + encodeURIComponent( searchterm ) + '&format=json', {
-		context: {
-			guildId: msg.guildId
+
+	const pagelink = wiki.toLink('Special:Search', { search: searchterm, fulltext: 1 });
+	let resultText = `<${pagelink}>`;
+	let description = [];
+	let footer = '';
+	let querypage = (Object.values((query.pages || {}))?.[0] || { title: '', ns: 0, invalid: '' });
+	const limit = searchLimit[(patreonGuildsPrefix.has(msg.guildId) ? 'patreon' : 'default')];
+
+	return got.get(`${wiki}api.php?action=query&titles=Special:Search&list=search&srinfo=totalhits&srprop=redirecttitle|sectiontitle&srnamespace=4|12|14|${(querypage.ns >= 0 ? querypage.ns + '|' : '')}${wiki.namespaces.content.map(ns => ns.id).join('|')}&srlimit=${limit}&srsearch=${encodeURIComponent(searchterm)}&format=json`, {
+		context: { guildId: msg.guildId }
+	}).then(response => {
+		const body = response.body;
+		if (body?.warnings) console.warn(body.warnings);
+		if (response.statusCode !== 200 || !body?.query?.search || body.batchcomplete === undefined) {
+			console.error('- Error while getting the search results:', body?.error?.info);
+			return;
 		}
-	} ).then( response => {
-		var body = response.body;
-		if ( body?.warnings ) log_warning(body.warnings);
-		if ( response.statusCode !== 200 || !body?.query?.search || body.batchcomplete === undefined ) {
-			return console.log( '- ' + response.statusCode + ': Error while getting the search results: ' + body?.error?.info );
-		}
-		if ( body.query.search.length < limit ) {
-			return got.get( wiki + 'api.php?action=query&list=search&srwhat=text&srinfo=totalhits&srprop=redirecttitle|sectiontitle&srnamespace=4|12|14|' + ( querypage.ns >= 0 ? querypage.ns + '|' : '' ) + wiki.namespaces.content.map( ns => ns.id ).join('|') + '&srlimit=' + limit + '&srsearch=' + encodeURIComponent( searchterm ) + '&format=json', {
-				context: {
-					guildId: msg.guildId
+		// If less than limit, do fallback search
+		if (body.query.search.length < limit) {
+			return got.get(`${wiki}api.php?action=query&list=search&srwhat=text&srinfo=totalhits&srprop=redirecttitle|sectiontitle&srnamespace=4|12|14|${(querypage.ns >= 0 ? querypage.ns + '|' : '')}${wiki.namespaces.content.map(ns => ns.id).join('|')}&srlimit=${limit}&srsearch=${encodeURIComponent(searchterm)}&format=json`, {
+				context: { guildId: msg.guildId }
+			}).then(tresponse => {
+				const tbody = tresponse.body;
+				if (tbody?.warnings) console.warn(tbody.warnings);
+				if (tresponse.statusCode !== 200 || !tbody?.query?.search || tbody.batchcomplete === undefined) {
+					console.error('- Error while getting text fallback results:', tbody?.error?.info);
+					return;
 				}
-			} ).then( tresponse => {
-				var tbody = tresponse.body;
-				if ( tbody?.warnings ) log_warning(tbody.warnings);
-				if ( tresponse.statusCode !== 200 || !tbody?.query?.search || tbody.batchcomplete === undefined ) {
-					return console.log( '- ' + tresponse.statusCode + ': Error while getting the text search results: ' + tbody?.error?.info );
+				body.query.search.push(...tbody.query.search.filter(tresult => {
+					return !body.query.search.some(result => result.pageid === tresult.pageid);
+				}).slice(0, limit - body.query.search.length));
+				if (body.query.searchinfo && tbody.query.searchinfo) {
+					body.query.searchinfo.totalhits += tbody.query.searchinfo.totalhits;
 				}
-				body.query.search.push(...tbody.query.search.filter( tresult => {
-					return !body.query.search.some( result => result.pageid === tresult.pageid );
-				} ).slice(0, limit - body.query.search.length));
-				if ( body.query.searchinfo && tbody.query.searchinfo ) body.query.searchinfo.totalhits += tbody.query.searchinfo.totalhits;
-			}, error => {
-				console.log( '- Error while getting the text search results: ' + error );
-			} ).then( () => {
-				return body;
-			} );
+			}).then(() => body);
 		}
 		return body;
-	} ).then( body => {
-		if ( !body?.query?.search ) return;
-		if ( body.query.pages?.['-1']?.title ) {
-			pagelink = wiki.toLink(body.query.pages['-1'].title, {search:searchterm,fulltext:1});
-			resultText = '<' + pagelink + '>';
-			if ( !noEmbed ) embed.setURL( pagelink );
-			else resultText += '\n\n**`' + searchterm + '`**';
+	}).then(body => {
+		if (!body?.query?.search) return;
+
+		if (body.query.pages?.['-1']?.title) {
+			resultText = `<${wiki.toLink(body.query.pages['-1'].title, { search: searchterm, fulltext: 1 })}>`;
 		}
-		var hasExactMatch = false;
-		var description = [];
-		body.query.search.forEach( result => {
+
+		let hasExactMatch = false;
+
+		body.query.search.forEach(result => {
 			let text = '• ';
 			let bold = '';
-			if ( result.title.replace( /[_-]/g, ' ' ).toLowerCase() === querypage.title.replaceAll( '-', ' ' ).toLowerCase() ) {
+
+			if (result.title.replace(/[_-]/g, ' ').toLowerCase() === querypage.title.replaceAll('-', ' ').toLowerCase()) {
 				bold = '**';
 				hasExactMatch = true;
-				if ( query.redirects?.[0] ) {
-					if ( query.redirects[0].tofragment && !result.sectiontitle ) {
+				if (query.redirects?.[0]) {
+					if (query.redirects[0].tofragment && !result.sectiontitle) {
 						result.sectiontitle = query.redirects[0].tofragment;
 					}
-					if ( !result.redirecttitle ) result.redirecttitle = query.redirects[0].from;
+					if (!result.redirecttitle) result.redirecttitle = query.redirects[0].from;
 				}
 			}
-			text += bold;
-			text += '[' + escapeFormatting(result.title) + '](<' + wiki.toLink(result.title, '', '', true) + '>)';
-			if ( result.sectiontitle ) {
-				text += ' § [' + escapeFormatting(result.sectiontitle) + '](<' + wiki.toLink(result.title, '', result.sectiontitle, true) + '>)';
+
+			text += bold + `[${escapeFormatting(result.title)}](<${wiki.toLink(result.title, '', '', true)}>)`;
+			if (result.sectiontitle) {
+				text += ` § [${escapeFormatting(result.sectiontitle)}](<${wiki.toLink(result.title, '', result.sectiontitle, true)}>)`;
 			}
-			if ( result.redirecttitle ) {
-				text += ' (⤷ [' + escapeFormatting(result.redirecttitle) + '](<' + wiki.toLink(result.redirecttitle, 'redirect=no', '', true) + '>))';
+			if (result.redirecttitle) {
+				text += ` (⤷ [${escapeFormatting(result.redirecttitle)}](<${wiki.toLink(result.redirecttitle, 'redirect=no', '', true)}>))`;
 			}
 			text += bold;
-			description.push( text );
-		} );
-		if ( !hasExactMatch ) {
-			if ( query.interwiki?.[0] ) {
-				let text = '• **⤷ ';
-				text += '__[' + escapeFormatting(query.interwiki[0].title) + '](<' + query.interwiki[0].url + '>)__';
-				if ( query.redirects?.[0] ) {
-					text += ' (⤷ [' + escapeFormatting(query.redirects[0].from) + '](<' + wiki.toLink(query.redirects[0].from, 'redirect=no', '', true) + '>))';
+
+			description.push(text);
+		});
+
+		if (!hasExactMatch) {
+			if (query.interwiki?.[0]) {
+				let text = `• **⤷ __[${escapeFormatting(query.interwiki[0].title)}](<${query.interwiki[0].url}>)__`;
+				if (query.redirects?.[0]) {
+					text += ` (⤷ [${escapeFormatting(query.redirects[0].from)}](<${wiki.toLink(query.redirects[0].from, 'redirect=no', '', true)}>))`;
 				}
 				text += '**';
-				description.unshift( text );
-			}
-			else if ( querypage.invalid === undefined && ( querypage.missing === undefined || querypage.known !== undefined ) ) {
-				let text = '• **';
-				text += '[' + escapeFormatting(querypage.title) + '](<' + wiki.toLink(querypage.title, '', '', true) + '>)';
-				if ( query.redirects?.[0] ) {
-					if ( query.redirects[0].tofragment ) {
-						text += ' § [' + escapeFormatting(query.redirects[0].tofragment) + '](<' + wiki.toLink(querypage.title, '', query.redirects[0].tofragment, true) + '>)';
+				description.unshift(text);
+			} else if (querypage.invalid === undefined && (querypage.missing === undefined || querypage.known !== undefined)) {
+				let text = `• **[${escapeFormatting(querypage.title)}](<${wiki.toLink(querypage.title, '', '', true)}>)`;
+				if (query.redirects?.[0]) {
+					if (query.redirects[0].tofragment) {
+						text += ` § [${escapeFormatting(query.redirects[0].tofragment)}](<${wiki.toLink(querypage.title, '', query.redirects[0].tofragment, true)}>)`;
 					}
-					text += ' (⤷ [' + escapeFormatting(query.redirects[0].from) + '](<' + wiki.toLink(query.redirects[0].from, 'redirect=no', '', true) + '>))';
+					text += ` (⤷ [${escapeFormatting(query.redirects[0].from)}](<${wiki.toLink(query.redirects[0].from, 'redirect=no', '', true)}>))`;
 				}
 				text += '**';
-				description.unshift( text );
+				description.unshift(text);
 			}
 		}
-		var footer = '';
-		if ( body.query.searchinfo ) {
+
+		if (body.query.searchinfo) {
 			footer = lang.get('search.results', body.query.searchinfo.totalhits.toLocaleString(lang.get('dateformat')), body.query.searchinfo.totalhits);
 		}
-		if ( !noEmbed ) {
-			if ( description.length ) embed.setDescription( splitMessage( description.join('\n') )[0] );
-			if ( footer ) embed.setFooter( {text: footer} );
+	}).then(() => {
+		// Create Components V2 Message
+		const components = [
+			new TextDisplayBuilder().setContent(`🔍 ${spoiler}${resultText}${spoiler}`),
+			new ContainerBuilder().addTextDisplayComponents(
+				...(description.length
+					? splitMessage(description.join('\n')).map(desc =>
+						new TextDisplayBuilder().setContent(desc)
+					)
+					: [new TextDisplayBuilder().setContent(lang.get('search.noresults'))]
+				)
+			)
+		];
+
+		if (footer) {
+			components.push(new TextDisplayBuilder().setContent(footer));
 		}
-		else {
-			if ( description.length ) resultText += '\n' + splitMessage( description.join('\n'), {maxLength: 1990 - resultText.length - footer.length} )[0];
-			if ( footer ) resultText += '\n' + footer;
-		}
+
+		return {
+			message: {
+				flags: MessageFlags.IsComponentsV2,
+				components
+			}
+		};
 	}, error => {
-		console.log( '- Error while getting the search results.' + error );
-	} ).then( () => {
-		return {message: {
-			content: '🔍 ' + spoiler + resultText + spoiler,
-			embeds: [embed]
-		}};
-	} );
+		console.error('- Error while getting search results:', error);
+	});
 }
